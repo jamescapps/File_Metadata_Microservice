725721528219662d4dffe403dfa9e2b84f55499e
James Capps <milk@pop-os.localdomain>
2019-12-12 12:38:32 -0600

    Working!

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 //Setup
 const express = require('express')
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
+const multer = require('multer')
+const bodyParser = require('body-parser')
+const upload = multer({dest: 'uploads/'})
 
 app.use(cors())
+app.use(bodyParser.json())
 app.use('/public', express.static(process.cwd() + '/public'))
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
  })
 
-app.post('/api/fileanalyse', (req, res) => {
-    console.log("Uploading...")
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+    if (!req.file) {
+        res.send("Please select a file.")
+    } else {
+        console.log("Uploading...")
+        res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size})
+    }
 })
 
 app.listen(process.env.PORT || 3000, function () {
